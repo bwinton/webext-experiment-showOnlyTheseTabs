0082ac4fc187fd36ecd302acd0ac12dfa40bccea
dietrich ayala <autonome@gmail.com>
2017-07-26 22:32:09 +0800

    initial commit

--- experiment/api.js
+++ experiment/api.js
@@ -1,9 +1,27 @@
+const {classes: Cc, interfaces: Ci, results: Cr, utils: Cu} = Components;
+
+
 class API extends ExtensionAPI {
   getAPI(context) {
     return {
-      hello: {
-        async hello() {
-          return "Hello, world!";
+      // Insert Experiment API here.
+      // Note: the namespace (boilerplate must match the id in the install.rdf)
+      showonlythesetabs: {
+        async showOnlyTheseTabs(tabIds) {
+
+          // Convert WE tabs into XUL tabs
+          var tabs = tabIds.map(function(tab) {
+            return extension.tabManager.get(tab.id, null);
+          });
+
+          // Get current browser window
+					var wm = Cc["@mozilla.org/appshell/window-mediator;1"]
+                   .getService(Ci.nsIWindowMediator);
+					var browserWindow = wm.getMostRecentWindow("navigator:browser");
+
+          // Set visible tabs
+          var tabbrowser = browserWindow.tabbrowser;
+          tabbrowser.showOnlyTheseTabs(tabs);
         }
       }
     };
--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,18 @@
-browser.hello.hello().then(
-  message => console.log(`hello sez: "${message}"`)
-);
+function hideAllOtherTabs() {
+
+	function onTabs(tabs) {
+		for (let tab of tabs) {
+			console.log('tab id', tab.id);
+  		browser.showonlythesetabs.showOnlyTheseTabs([tab.id]);
+		}
+	}
+
+	function onError(error) {
+		console.log(`Error: ${error}`);
+	}
+
+	var querying = browser.tabs.query({currentWindow: true, active: true});
+	querying.then(onTabs, onError);
+}
+
+browser.browserAction.onClicked.addListener(hideAllOtherTabs);
